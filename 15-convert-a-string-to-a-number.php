222b68449483886adb81951d34f0e0bf159cc74b
kuwadikadun <kuwadikadun@gmail.com>
2024-07-24 23:05:28 +0700

    Finish: 15-16

---
+++ 15-convert-a-string-to-a-number.php
@@ -0,0 +1,22 @@
+<?php
+
+function stringToNumber($str) {
+    return (int)$str;
+  }
+
+stringToNumber('1234');
+
+
+
+
+
+
+
+
+
+// The Unique Answer
+
+// function stringToNumber($str) {
+//     // do stuff
+//     return intval($str);
+//   }
---
+++ 16-function-1-hello-world.php
@@ -0,0 +1,8 @@
+<?php
+
+// Write a function `greet` that returns "hello world!"
+function greet() {
+    return 'hello world!';
+}
+
+greet();
